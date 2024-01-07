65395166ae66360ee9370267c44cbae9f94117f7
poludin <poludin@gmail.com>
2024-01-07 21:47:10 +0300

    Добавлены airflow dags

---
+++ Задание_1/Задача 1.1/airflow_dags/migrate_data.py
@@ -0,0 +1,17 @@
+import time
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+data = DAG(
+	dag_id = 'data_migration', 				#Уникальный идентификатор DAG
+	schedule_interval = "*/20 * * * *",  	#Интервал выполнения - каждые 20 минут
+	start_date = datetime(2024, 1, 6), 		#Определяет дату, с которых начинается выполнение DAG
+	catchup = False) 						#Определяет, должны ли быть выполнены пропущенные задачи, если DAG запускается с отставанием
+
+migrate = BashOperator(
+	task_id = 'migrate', 					#Уникальный идентификатор задачи
+	bash_command ='sh -x /home/bizzy/airflow/dags/read_data_talent_to_postgresql/ns_petproject_build_job/ns_petproject/ns_petproject_run.sh ',
+	dag = data)								#Передеча параметров о времени запуска
+
+migrate 									#Вызов задачи
