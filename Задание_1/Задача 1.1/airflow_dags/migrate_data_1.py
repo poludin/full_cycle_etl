02fa25042e75106552cce8f1e9e38dff097a4906
poludin <poludin@gmail.com>
2024-01-08 23:14:58 +0300

    Update migrate_data_1.py

--- Задание_1/Задача 1.1/airflow_dags/migrate_data_1.py
+++ Задание_1/Задача 1.1/airflow_dags/migrate_data_1.py
@@ -3,24 +3,24 @@ from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
-default_args = {							#При создании DAG, устанавливает эти значения по умолчанию для всех задач в DAG
-    'owner': 'airflow', 					#Определяет владельца DAG
+default_args = {					#При создании DAG, устанавливает эти значения по умолчанию для всех задач в DAG
+    'owner': 'airflow', 				#Определяет владельца DAG
     'depends_on_past': False,				#Определяет, зависят ли задачи в DAG от выполнения предыдущих задач
-    'start_date': datetime(2024, 1, 6),		#Определяет дату, с которых начинается выполнение DAG
-    'retries': 1,							#Определяет количество попыток повторного выполнения задачи в случае ошибки
-	'retry_delay': timedelta(minutes = 2)	#Определяет интервал между повторными попытками выполнения задачи в случае ошибки, задача будет повторно выполнена через 5 минут после ошибки
+    'start_date': datetime(2024, 1, 6),			#Определяет дату, с которых начинается выполнение DAG
+    'retries': 1,					#Определяет количество попыток повторного выполнения задачи в случае ошибки
+	'retry_delay': timedelta(minutes = 2)		#Определяет интервал между повторными попытками выполнения задачи в случае ошибки, задача будет повторно выполнена через 5 минут после ошибки
 }
 
 test_dag = DAG(
-    'data_migration_1',						#Уникальный идентификатор DAG
+    'data_migration_1',					#Уникальный идентификатор DAG
     default_args = default_args,			#Передает настройки по умолчанию для DAG
     schedule_interval = '@hourly'			#Интервал выполнения - каждый час
 )
 
 bash_task = BashOperator(
-    task_id = 'bash_execute_script',		#Уникальный идентификатор задачи
-    bash_command = 'sh -x /home/bizzy/airflow/dags/read_data_talent_to_postgresql/ns_petproject_build_job/ns_petproject/ns_petproject_run.sh ',
-    dag = test_dag							#Передеча параметров о времени запуска
+    task_id = 'bash_execute_script',			#Уникальный идентификатор задачи
+    bash_command = 'sh -x ./ns_petproject_run.sh ',
+    dag = test_dag					#Передеча параметров о времени запуска
 )
 
-bash_task									#Вызов задачи
+bash_task						#Вызов задачи
