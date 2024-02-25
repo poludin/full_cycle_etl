0742d0b56cb6ce4928e93e3f97df63b694a7d9b1
poludin <poludin@gmail.com>
2024-02-25 20:49:28 +0300

    Update task_1_1.sql

--- Задание_1/Задача 1.1/sql_scripts/task_1_1.sql
+++ Задание_1/Задача 1.1/sql_scripts/task_1_1.sql
@@ -71,8 +71,8 @@ grant all privileges on all tables in schema ds to ds;
 create table ds.ft_balance_f (
 	on_date 	date not null,
 	account_rk 	int not null,
-	currency_rk int,
-	balance_out float
+	currency_rk 	int,
+	balance_out 	float
 );
 
 alter table ds.ft_balance_f add primary key (on_date, account_rk);
@@ -86,7 +86,7 @@ select * from ds.ft_balance_f order by account_rk;
  */
 
 create table ds.ft_posting_f (
-	oper_date 			date not null,
+	oper_date 		date not null,
 	credit_account_rk 	int not null,
 	debet_account_rk 	int not null,
 	credit_amount 		float,
@@ -104,13 +104,13 @@ select * from ds.ft_posting_f order by oper_date;
  */
 
 create table ds.md_account_d (
-	data_actual_date 		date not null,
+	data_actual_date 	date not null,
 	data_actual_end_date 	date not null,
-	account_rk 				int not null,
-	account_number 			varchar(20) not null,
-	char_type 				varchar(1) not null,
-	currency_rk 			int not null,
-	currency_code 			varchar(3) not null
+	account_rk 		int not null,
+	account_number 		varchar(20) not null,
+	char_type 		varchar(1) not null,
+	currency_rk 		int not null,
+	currency_code 		varchar(3) not null
 );
 
 alter table ds.md_account_d add primary key (data_actual_date, account_rk);
@@ -124,11 +124,11 @@ select * from ds.md_account_d mad order by account_rk;
  */
 
 create table ds.md_currency_d (
-	currency_rk 			int not null,
-	data_actual_date 		date not null,
+	currency_rk 		int not null,
+	data_actual_date 	date not null,
 	data_actual_end_date 	date,
-	currency_code 			varchar(3),
-	code_iso_char 			varchar(3)
+	currency_code 		varchar(3),
+	code_iso_char 		varchar(3)
 );
 
 alter table ds.md_currency_d add primary key (currency_rk, data_actual_date);
@@ -142,11 +142,11 @@ select * from ds.md_currency_d order by currency_rk;
  */
 
 create table ds.md_exchange_rate_d (
-	data_actual_date 		date not null,
+	data_actual_date 	date not null,
 	data_actual_end_date 	date,
-	currency_rk 			int not null,
-	reduced_cource 			float,
-	code_iso_num 			varchar(3)
+	currency_rk 		int not null,
+	reduced_cource 		float,
+	code_iso_num 		varchar(3)
 );
 
 alter table ds.md_exchange_rate_d add primary key (data_actual_date, currency_rk);
@@ -160,34 +160,34 @@ select * from ds.md_exchange_rate_d order by currency_rk;
  */
 
 create table ds.md_ledger_account_s (
-	chapter 						char(1),
-	chapter_name 					varchar(16),
-	section_number 					int,
-	section_name 					varchar(22),
-	subsection_name 				varchar(21),
-	ledger1_account 				int,
+	chapter 				char(1),
+	chapter_name 				varchar(16),
+	section_number 				int,
+	section_name 				varchar(22),
+	subsection_name 			varchar(21),
+	ledger1_account 			int,
 	ledger1_account_name 			varchar(47),
-	ledger_account 					int not null,
+	ledger_account 				int not null,
 	ledger_account_name 			varchar(153),
-	characteristic 					char(1),
-	is_resident 					int,
-	is_reserve 						int,
-	is_reserved 					int,
-	is_loan 						int,
-	is_reserved_assets 				int,
-	is_overdue 						int,
-	is_interest 					int,
-	pair_account 					varchar(5),
-	start_date 						date not null,
-	end_date 						date,
-	is_rub_only 					int,
-	min_term 						varchar(1),
-	min_term_measure 				varchar(1),
-	max_term 						varchar(1),
-	max_term_measure 				varchar(1),
-	ledger_acc_full_name_translit 	varchar(1),
-	is_revaluation 					varchar(1),
-	is_correct 						varchar(1)
+	characteristic 				char(1),
+	is_resident 				int,
+	is_reserve 				int,
+	is_reserved 				int,
+	is_loan 				int,
+	is_reserved_assets 			int,
+	is_overdue 				int,
+	is_interest 				int,
+	pair_account 				varchar(5),
+	start_date 				date not null,
+	end_date 				date,
+	is_rub_only 				int,
+	min_term 				varchar(1),
+	min_term_measure 			varchar(1),
+	max_term 				varchar(1),
+	max_term_measure 			varchar(1),
+	ledger_acc_full_name_translit 		varchar(1),
+	is_revaluation 				varchar(1),
+	is_correct 				varchar(1)
 );
 
 alter table ds.md_ledger_account_s add primary key (ledger_account, start_date);
